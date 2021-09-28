a2bd9a3b678f789d1c4b26852eac28d8764ff402
ShantanuMittalUnthinkable <shantanu.mittal@unthinkable.co>
2021-09-28 20:13:35 +0530

    Linked List

--- training/apps/assignment_5/q3.py
+++ training/apps/assignment_5/q3.py
@@ -8,7 +8,7 @@ class Node:
 
 class LinkedList:
 
-    def __init__(self, first: Node) -> None:
+    def __init__(self, first: Node = None) -> None:
         
         self.first = first
 
@@ -33,4 +33,34 @@ class LinkedList:
     
     def add_node(self, key: int, incoming_node: Node) -> None:
 
-        pass
+        '''
+        To add a node after a key
+        '''
+
+        target_node = None
+
+        p = self.first
+
+        while(p.data != key):
+            p = p.next_node
+
+        if p.data != key:
+            raise Exception("Key not present in linked list.")
+
+        incoming_node.next_node = p.next_node
+        p.next_node = incoming_node
+
+    def delete_node(self, key: int):
+
+        previous_node = None
+
+        p = self.first
+
+        while(p.data != key):
+            previous_node = p
+            p = p.next_node
+
+        if p.data != key:
+            raise Exception("Key not present in linked list.")
+        
+        previous_node.next_node = p.next_node
